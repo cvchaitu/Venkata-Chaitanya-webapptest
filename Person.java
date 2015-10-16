e5ea5ab8b43a3cc569070118c3fcdd10d1c01b0d
cvchaitu <cvchaitanyareddy@gmail.com>
2015-10-15 21:32:35 -0500

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,17 @@
+package com.proquest.interview.phonebook;
+
+public class Person {
+
+	public Person (String name, String phoneNumber,String address){
+		this.name = name;
+		this.phoneNumber= phoneNumber;
+		this.address=address;
+		
+	}
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+	public String name;
+	public String phoneNumber;
+	public String address;
+}
